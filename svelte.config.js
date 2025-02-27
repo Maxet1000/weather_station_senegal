50ae173f3a5d966835447c433f4407d90f39597a
Maxet1000 <maxemile.meylaerts@gmail.com>
2025-02-27 23:54:42 +0100

    changed config to integrate with cloudflare part 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,20 @@
-import adapter from '@sveltejs/adapter-cloudflare'; // Import directly
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter({
-      // See below for options, add them here
-      routes: {
-        include: ['/*'],
-        exclude: ['<all>']
-      }
-    }),
-    output: {
-      dir: 'build' // Not needed here. adapter-cloudflare controls this.
-    }
+	preprocess: vitePreprocess(),
 
-  }
+	kit: {
+		adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            routes: {
+                include: ['/*'],
+                exclude: ['<all>']
+            }
+        })
+	}
 };
 
 export default config;
