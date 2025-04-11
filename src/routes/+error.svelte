ce11e4d97cdf0865733eb503ffe66891ada94ac8
Maxet1000 <maxemile.meylaerts@gmail.com>
2025-04-11 20:48:47 +0200

    added error page image to production version

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="container">
-  <img src="../src//images/404Senegalcropped.png" alt="404" class="image" />
+  <img src="/images/404Senegalcropped.png" alt="404" class="image" />
   <div class="text">Page Not Found </div>
   <a class="button" href="/"> Go to Homepage </a>
 </div>
